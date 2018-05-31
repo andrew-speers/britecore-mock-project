73fcb7ba4d6b021673a5f1a96c0de2941de819d7
andrew-speers <andrew@Gamebox.localdomain>
2018-05-31 17:28:12 -0400

    Added env to spec

--- app.py
+++ app.py
@@ -2,7 +2,22 @@
 from http.server import BaseHTTPRequestHandler,HTTPServer
 from os import curdir,sep
 import psycopg2
-
+'''
+try:
+    connect_str = "dbname='earnest-vent-205713:northamerica-northeast1:d6539' user='postgres' host='35.203.43.129' " + \
+                  "port='3306' password='AswuEGlq7AN57Lyl'"
+    # use our connection values to establish a connection
+    conn = psycopg2.connect(connect_str)
+    # create a psycopg2 cursor that can execute queries
+    cursor = conn.cursor()
+    # run a SELECT statement - no data in there, but we can try it
+    cursor.execute("""SELECT * from requests""")
+    rows = cursor.fetchall()
+    print(rows)
+except Exception as e:
+    print("Uh oh, can't connect. Invalid dbname, user or password?")
+    print(e)
+'''
 PORT_NUMBER = 8080
 
 class myHandler(BaseHTTPRequestHandler):
