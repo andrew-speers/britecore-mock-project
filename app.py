56c3cb836414da0078135bde83f05eeef803a676
andrew-speers <andrew@Gamebox.localdomain>
2018-06-01 17:25:48 -0400

    Changed dbname again

--- app.py
+++ app.py
@@ -6,9 +6,10 @@ import psycopg2
 log = open('/var/log/test.log', 'w')
 
 log.write("Restarting...")
+log.write(environ['DB_USER'])
 try:
     conn = psycopg2.connect(
-        database='d6539',
+        database='earnest-vent-205713:northamerica-northeast1:d6539',
         user=environ['DB_USER'],
         password=environ['DB_PASSWORD'],
         host='127.0.0.1',
